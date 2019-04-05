eaa3d16a97f1e137beccada07045d3b148045c60
tqian1 <t.qian7@gmail.com>
2019-04-05 13:26:46 -0600

    got post and get requests working for features

--- SentryApp/client/app/admin/admin.controller.js
+++ SentryApp/client/app/admin/admin.controller.js
@@ -2,15 +2,45 @@
 
 export default class AdminController {
   users: Object[];
+  features: Object[];
+  featureText = "";
+  featureInfo = "";
 
   /*@ngInject*/
-  constructor(User) {
+  constructor(User, $http) {
     // Use the User $resource to fetch all users
     this.users = User.query();
+    this.$http = $http;
+    this.getFeatures();
   }
 
   delete(user) {
     user.$remove();
     this.users.splice(this.users.indexOf(user), 1);
+  }
+
+  addFeature() {
+    let data = {
+      name: this.featureText,
+      info: this.featureInfo,
+    };
+    this.$http.post('/api/things/', data)
+    .then(res => {
+        this.getFeatures();
+        console.log("success");
+      }, err => {
+      window.alert(err);
+    });
+  }
+
+  getFeatures() {
+    this.$http.get('/api/things', {
+    })
+    .then(res => {
+      this.features = res.data;
+      console.log(this.features);
+    }, err => {
+      window.alert('Couldn\'t fetch orders', err);
+    });
   }
 }
